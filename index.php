b4a8ec8ec9a748c44005817e42f453a6530bde00
elvinmammadoff <elvinmammadoff@gmail.com>
2018-09-05 20:05:30 +0400

    added twitter card to index page

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 
     <!-- Twitter Card -->
     <meta name="twitter:card" content="summary"></meta>   
-    <meta name="twitter:site" content="@nytimesbits" />
-    <meta name="twitter:creator" content="@nickbilton" />
+    <meta name="twitter:site" content="@elvin_mammadoff" />
+    <meta name="twitter:creator" content="@elvin_mammadoff" />
     <meta property="og:url" content="https://rafoz-farzo.herokuapp.com/" />
     <meta property="og:title" content="A Twitter for My Sister" />
     <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
