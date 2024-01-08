a66f3458bed6e440806de1746bbd0d3a23208305
Elvin Mammadov <elvinmammadoff@gmail.com>
2024-01-09 02:54:50 +0400

    v2

---
+++ landing/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- BEGIN head -->
+<head>
+
+    <!-- Meta tags -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Rafoz Farzo - Photography Template</title>
+
+    <!-- Stylesheets -->
+    <link href="assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" media="all">
+    <link href="assets/css/fonts.css" type="text/css" rel="stylesheet">
+    <link href="assets/css/style.css" type="text/css" rel="stylesheet">
+    <link href="assets/css/media.css" type="text/css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="256x256"  href="assets/img/favicon/android-chrome-256x256.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/favicon/android-chrome-192x192.png">    
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon/favicon.ico">
+    <link rel="manifest" href="assets/img/site.webmanifest">
+    <link rel="mask-icon" href="assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#990100">
+    <meta name="theme-color" content="#ffffff">
+
+</head>
+<!-- END head -->
+
+<body>
+    <div class="banner" id="banner">
+        <div class="banner-inner">
+            <div class="container">
+                <img src="assets/img/logo.png" alt="logo" class="logo">
+                <h1 class="title upper"><span>Photography Template</span></h1>
+                <p class="subtitle">
+                    Rafoz Farzo is the best photography template on the market to help photographers, bloggers. Looks good on all types of screens devices and platforms.
+                    Documented well and very easy to customize & does not require much knowledge of coding.
+                </p>
+                <a href="#pages" class="button upper page-scroll">View Demos</a>
+            </div>
+        </div>
+    </div>
+    <div class="container section" id="pages">
+        <div class="section-subject">
+            <h5 class="section-category">Home Pages</h5>
+            <h2 class="section-title">2 Unique Home Pages</h2>
+            <p class="section-subtitle">We built each home page with a different style so that there is something for everyone.</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <div class="demo">
+                    <a href="https://meek-pegasus-08eed8.netlify.app/" class="demo-item">
+                        <div class="demo-inner page-1"></div>
+                    </a>
+                    <h4 class="demo-title">Home Version 1</h4>
+                    <a href="https://meek-pegasus-08eed8.netlify.app/" class="button">View Page</a>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 col-lg-6">
+                <div class="demo">
+                    <a href="https://meek-pegasus-08eed8.netlify.app/home-v2" class="demo-item">
+                        <div class="demo-inner page-2"></div>
+                    </a>
+                    <h4 class="demo-title">Home Version 2</h4>
+                    <a href="https://meek-pegasus-08eed8.netlify.app/home-v2" class="button">View Page</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <p class="copyright-text">Copyright © 2024 Alissio All Rights Reserved.</p>
+        </div>
+    </div>
+
+    <!-- JavaScript -->
+    <script src="assets/js/jquery-2.1.1.min.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html> 
